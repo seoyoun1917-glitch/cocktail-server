a055a3bc4dff8d6bdaffc55f549a0a49a00df541
seoyoun1917-glitch <seoyoun1917@gmail.com>
2026-04-16 18:17:43 +0900

    26.04.16

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+// 미들 웨어 : 요청과 라우터 사이에 실행되는 함수
+const authMiddleware = (req, res, nezt) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+  // 토큰이 유효한지 검사 > 안에 담긴 정보 꺼냄
+  // JWT.verify(토큰, 비밀키)
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  req.userId = decoded.userId;
+  // 통과헀고 다음 요청으로 이동
+  next();
+};
+export default authMiddleware;
